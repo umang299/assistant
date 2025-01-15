ff93854a1f9cf49908cba202ece5a839bf07f0aa
umang299 <umang91999@gmail.com>
2025-01-15 19:00:29 +0530

    mod: added logging to the utility functions

--- src/utils.py
+++ src/utils.py
@@ -15,70 +15,124 @@ openai_ef = embedding_functions.OpenAIEmbeddingFunction(
             )
 
 
-def read_yaml(file_path):
+def read_yaml(file_path, logger=None):
     """
     Reads data from a YAML file and returns it as a dictionary.
     
-    :param file_path: Path to the YAML file.
-    :return: Parsed data from the YAML file as a dictionary or list.
+    Args:
+        file_path: Path to the YAML file.
+    
+    Return:
+        Parsed data from the YAML file as a dictionary or list.
     """
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r', encoding='utf-8') as file:
             data = yaml.safe_load(file)
             return data
     except FileNotFoundError:
-        print(f"Error: File not found at {file_path}")
-    except yaml.YAMLError as e:
-        print(f"Error parsing YAML file: {e}")
-    except Exception as e:
-        print(f"Unexpected error: {e}")
-
-
-
-def load_conversation(thread_id):
-    conn = sqlite3.connect(os.path.join(cwd, 'ckpt.sqlite'))
-    cursor = conn.cursor()
-
-    query = f"""
-        SELECT thread_id, checkpoint
-        FROM checkpoints
-        WHERE thread_id = '{thread_id}'
-        """
-    
-    cursor.execute(query)
-    resp = cursor.fetchall()
-    conn.close()
-    
-    byte_stream = resp[-1][1]
-    rep = msgpack.unpackb(byte_stream, raw=False)
-    channel_vals = rep['channel_values']['messages']
-
-    history = list()
-    for vals in channel_vals:
-        channel_rep = msgpack.unpackb(vals.data, raw=False)
-        msg = channel_rep[2]['content']
-        type_ = channel_rep[1]
-
-        if type_ == 'HumanMessage':
-            temp = {
-                'role' : 'user',
-                'content' : msg
-            }
-            history.append(temp)
-        elif type_ == 'AIMessage' and len(msg) != 0 :
-            temp = {
-                'role' : 'assistant',
-                'content' : msg
-            }
-            history.append(temp)
+        if logger is not None:
+            logger.info("read_yaml: File not found at %s", file_path)
         else:
-            pass
-        
-    return history
+            print("read_yaml: File not found at %s", file_path)
+    except yaml.YAMLError as e:
+        if logger is not None:
+            logger.info("read_yaml: Error parsing YAML file -> %s", e)
+        else:
+            print("read_yaml: Error parsing YAML file -> %s", e)
+    except Exception as e:
+        if logger is not None:
+            logger.info("read_yaml: Unexpected error -> %s", e)
+        else:
+            print("read_yaml: Unexpected error -> %s", e)
 
 
-def split_and_chunk(docs, languages: list = ['.py', '.md', '.sh']):
+def connect2sqlite(logger=None):
+    """
+    Function to connect to sqlite DB. 
+    """
+    try:
+        db_path = os.path.join(cwd, 'ckpt.sqlite')
+        conn = sqlite3.connect(database=db_path)
+        cursor = conn.cursor()
+        if logger is not None:
+            logger.info('connect2sqlite: Connected to %s', db_path)
+        else:
+            print('connect2sqlite: Connected to %s', db_path)
+        return cursor, conn
+    except Exception as e:
+        if logger is not None:
+            logger.info('connect2sqlite: Failed to connect -> %s', e)
+        else:
+            print('connect2sqlite: Failed to connect -> %s', e)
+        return None, None
 
+def load_conversation(thread_id, logger=None):
+    """
+    Function to load chat history.
+    """
+    try:
+        cursor, conn = connect2sqlite(logger=logger)
+        query = f"""
+            SELECT thread_id, checkpoint
+            FROM checkpoints
+            WHERE thread_id = '{thread_id}'
+            """
+
+        if cursor is not None:
+            cursor.execute(query)
+            resp = cursor.fetchall()
+            conn.close()
+
+            byte_stream = resp[-1][1]
+            rep = msgpack.unpackb(byte_stream, raw=False)
+            channel_vals = rep['channel_values']['messages']
+
+            history = list()
+            for vals in channel_vals:
+                channel_rep = msgpack.unpackb(vals.data, raw=False)
+                msg = channel_rep[2]['content']
+                type_ = channel_rep[1]
+
+                if type_ == 'HumanMessage':
+                    temp = {
+                        'role' : 'user',
+                        'content' : msg
+                    }
+                    history.append(temp)
+                elif type_ == 'AIMessage' and len(msg) != 0 :
+                    temp = {
+                        'role' : 'assistant',
+                        'content' : msg
+                    }
+                    history.append(temp)
+                else:
+                    pass
+            if logger is not None:
+                logger.info("load_conversation: Found chat history with %s exchanges at thread_id %s",
+                            len(history),
+                            thread_id
+                            )
+            else:
+                print("load_conversation: Found chat history with %s exchanges at thread_id %s",
+                      len(history), 
+                      thread_id
+                    )
+            return history
+    except Exception as e:
+        if logger is not None:
+            logger.info("load_conversation: Failed to load chat history for thread_id %s -> %s",
+                        thread_id, 
+                        e
+                        )
+        else:
+            print("load_conversation: Failed to load chat history for thread_id %s -> %s",
+                  thread_id, 
+                  e
+                )
+        return None
+
+
+def split_and_chunk(docs, languages: list = ['.py', '.md', '.sh'], logger=None):
     splitter_cfg = {
         'chunk_lines' : 100,
         'chunk_lines_overlap' : 25
