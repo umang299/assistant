6e7148a4767c622f38c341c48f7886a775a3dc02
umang299 <umang91999@gmail.com>
2025-01-16 00:53:59 +0530

    mod: added logger to the agent graph

--- src/clients/agent.py
+++ src/clients/agent.py
@@ -26,18 +26,26 @@ class ExecutionGraph:
     LLM with vector DB tool.
     """
     def __init__(self,
-                 config: GraphConfig
+                 config: GraphConfig,
+                 logger = None
                  ):
-        
+
         self.config = config
+        self.logger = logger
         self.sys_msg = SystemMessage(
             content="""You are a helpful assistant tasked with helping the \
                 user breakdown complex implementations of large code bases.""")
 
         self.llm_with_tools = self.__initialize_llm()
         self.graph = self.build_graph()
-    
-    def build_prompt(self, state):
+
+        if any(x is None for x in [self.llm_with_tools, self.graph]):
+            if self.logger is not None:
+                self.logger.error('Agent Initialization Failed')
+            else:
+                print('Agent Initialization Failed')
+
+    def __build_prompt(self, state):
         chat_history = list()
         for msg in state['messages']:
             if msg.type == 'ai':
@@ -48,13 +56,13 @@ class ExecutionGraph:
                 chat_history.append(temp)
             else:
                 pass
-        
+
         template = ChatPromptTemplate(
                     messages=[self.sys_msg,
                               ("placeholder", "{conversation}")
                     ]
                 )
-        
+
         prompt = template.invoke({'conversation' : chat_history})
         return prompt
 
@@ -70,16 +78,32 @@ class ExecutionGraph:
         """
         Function to initialize LLM and bind tools to it.
         """
-        llm = ChatOpenAI(model=self.config.model_name)
-        llm_with_tools = llm.bind_tools(tools=self.config.tools)
-        return llm_with_tools
+        try:
+            llm = ChatOpenAI(model=self.config.model_name)
+            llm_with_tools = llm.bind_tools(tools=self.config.tools)
+
+            if self.logger is not None:
+                self.logger.info(f"initialize_llm: Initialized {self.config.model_name}")
+            else:
+                print(f"initialize_llm: Initialized {self.config.model_name}")
+
+            return llm_with_tools
+        except Exception as e:
+            if self.logger is not None:
+                self.logger.error('initialize_llm: Failed to initialize model %s',
+                                 e)
+            else:
+                print('initialize_llm: Failed to initialize model %s',
+                        e
+                    )
+            return None
 
     def __assitant_node(self, state: MessagesState):
         """
         Assistant node of the graph. This invokes the LLM with a system message and
         current state message to generate a response.
         """
-        prompt = self.build_prompt(state=state)
+        prompt = self.__build_prompt(state=state)
         return {"messages": [self.llm_with_tools.invoke(input=prompt)]}
 
     def build_graph(self):
@@ -87,18 +111,33 @@ class ExecutionGraph:
         Build the execution graph. This has two nodes the assistant node to call tools.
         Tools node to execute function calls from the llm.
         """
-        builder = StateGraph(MessagesState)
-        builder.add_node("assistant", self.__assitant_node)
-        builder.add_node("tools", ToolNode(self.config.tools))
+        try:
+            builder = StateGraph(MessagesState)
+            builder.add_node("assistant", self.__assitant_node)
+            builder.add_node("tools", ToolNode(self.config.tools))
 
-        builder.add_edge(START, "assistant")
-        builder.add_conditional_edges(
-            "assistant",
-            tools_condition
-        )
-        builder.add_edge("tools", "assistant")
-        graph = builder.compile(checkpointer=self.__state_checkpoint())
-        return graph
+            builder.add_edge(START, "assistant")
+            builder.add_conditional_edges(
+                "assistant",
+                tools_condition
+            )
+            builder.add_edge("tools", "assistant")
+            graph = builder.compile(checkpointer=self.__state_checkpoint())
+
+            if self.logger is not None:
+                self.logger.info('build_graph: Successfully built Agent Graph')
+            else:
+                print('build_graph: Successfully built Agent Graph')
+
+            return graph
+        except Exception as e:
+            if self.logger is not None:
+                self.logger.info('build_graph: Failed to built Agent Graph -> %s',
+                                e)
+            else:
+                print('build_graph: Failed to built Agent Graph -> %s',
+                                e)
+            return None
 
     def invoke(self, thread_id, message):
         """
