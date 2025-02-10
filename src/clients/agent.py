c95f980a16a3f0e8cb14118d9924225e65755729
umang299 <umang91999@gmail.com>
2025-02-11 00:29:47 +0530

    mod: added chief analyst agent

--- src/clients/agent.py
+++ src/clients/agent.py
@@ -9,15 +9,18 @@ from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
 
 from langgraph.graph import MessagesState
-from langgraph.graph import StateGraph, START
+from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
 from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.checkpoint.memory import MemorySaver
 
 
-cwd = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+cwd = os.path.realpath(os.path.join(os.path.dirname(os.path.dirname(__file__)), '..'))
 sys.path.append(cwd)
 
+from src.utils import read_text_file
 from src.helper.dataloader import GraphConfig
+from src.helper.states import GenerateAnalyst, Perspectives
 
 
 class ExecutionGraph:
@@ -27,7 +30,7 @@ class ExecutionGraph:
     def __init__(self,
                  config: GraphConfig
                  ):
-        
+
         self.config = config
         self.sys_msg = SystemMessage(
             content="You are a helpful assistant tasked with helping the \
@@ -85,3 +88,93 @@ class ExecutionGraph:
         msg = [HumanMessage(content=message)]
         messages = self.graph.invoke({"messages": msg}, config)
         return messages['messages'][-1].content
+
+
+
+class ChiefAnalyst:
+    """
+    Chief Analyst Graph. Responsible to build a series of Analysts.
+    """
+    def __init__(self):
+        self.prompt = read_text_file(
+                            file_path=os.path.join(
+                                        cwd,
+                                        'src',
+                                        'prompts', 
+                                        'chief_analyst.txt'
+                                    )
+                            )
+        self.llm = ChatOpenAI(model='gpt-3.5-turbo')
+        self.graph = self.build_graph()
+
+    def assistant(self, state: GenerateAnalyst):
+        """
+        Assitant to generate AI analysts.
+        """
+        topic  = state.get('topic', '')
+        feedback = state.get('human_analyst_feedback', '')
+        max_analyst = state.get('max_analyst', '')
+
+        llm_with_structed_op = self.llm.with_structured_output(Perspectives)
+        sys_msg = self.prompt.format(topic=topic,
+                                     human_analyst_feedback=feedback,
+                                     max_analysts=max_analyst)
+
+        analysts = llm_with_structed_op.invoke(
+                                [SystemMessage(content=sys_msg)] +
+                                [HumanMessage(content='Generate the set of analysts')]
+        )
+
+        return {'analysts' :  analysts.analysts}
+
+    def human_feedback(self, state: GenerateAnalyst):
+        pass
+
+    def should_continue(self, state: GenerateAnalyst):
+        """
+        Return the next node to execute
+        """
+        human_analyst_feedback = state.get('human_analyst_feedback', None)
+        if human_analyst_feedback:
+            return 'chief_analyst'
+        return END
+
+    def build_graph(self):
+        """
+        Function to build chief analyst agent.
+        """
+        builder = StateGraph(state_schema=GenerateAnalyst)
+        builder.add_node('chief_analyst', self.assistant)
+        builder.add_node('human_feedback', self.human_feedback)
+
+        builder.add_edge(START, 'chief_analyst')
+        builder.add_edge('chief_analyst', 'human_feedback')
+        builder.add_conditional_edges(
+                                    'human_feedback', 
+                                    self.should_continue, 
+                                    ['chief_analyst', END]
+                                )
+
+        graph = builder.compile(checkpointer=MemorySaver())
+        return graph
+
+
+    def run(self,
+            thread_id: str,
+            max_analyst: int,
+            topic: str,
+            instructions: str = None
+        ):
+        """
+        Function to run chief analyst agent
+        """
+        config = {"configurable": {"thread_id": thread_id}}
+        resp = self.graph.invoke(
+                        {
+                            'topic' : topic, 
+                            'max_analyst' : max_analyst,
+                            'human_analyst_feedback' : instructions
+                        },
+                    config=config
+                )
+        return resp
